da4af7580c2f410d195a486ed679414386ccddd6
Vinícius Martins <viniciusmartinsg@hotmail.com>
2022-07-17 09:30:23 -0300

    update(logi): added alerts

--- PostIt/src/app/pages/login/login.page.ts
+++ PostIt/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 import { LoginPayload } from '../../models/payloads/login.payload';
 
 @Component({
@@ -8,7 +9,10 @@ import { LoginPayload } from '../../models/payloads/login.payload';
 })
 export class LoginPage {
 
-  constructor() { }
+  constructor(
+    private readonly toastController: ToastController,
+    private readonly alertController: AlertController,
+  ) { }
 
   public loginPayload: LoginPayload = {
     email: '',
@@ -17,9 +21,26 @@ export class LoginPage {
 
   public isLoading: boolean = false;
 
-  public login(): void {
+  public async login(): Promise<void> {
     this.isLoading = true;
     console.log(this.loginPayload);
+
+    const toast = await this.toastController.create({
+      message: 'Logando',
+      duration: 2000,
+    });
+    await toast.present();
+
+    const alert = await this.alertController.create({
+      header: 'Logando',
+      buttons: [
+        {
+          text: 'Ok',
+          handler: () => { console.log('Ok!');},
+        },
+      ],
+    });
+    await alert.present();
   }
 
   public canLogin(): boolean {
